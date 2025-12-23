bad371eca55051b9733e45c6a142088604de9ad2
Amogh Pai <amogh1924@gmail.com>
2025-12-24 02:27:12 +0530

    js exerises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,num) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] === num) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
